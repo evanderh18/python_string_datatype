6299a4bd2da5f00eb02e895f028d0e3b32ce7040
evanderh18 <113483413+evanderh18@users.noreply.github.com>
2022-09-27 18:09:13 -0600

    Created variable for first and last name

--- python_strings.py
+++ python_strings.py
@@ -2,12 +2,16 @@
 #   - Create the following variables
 #   - my_first_name
 #       -set this equal to your first name
+my_first_name = 'Evan'
 #   - my_last_name
 #       -set this equal to your last name
+my_last_name = 'VanderHoeven'
 #   - my_year_of_birth
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
+my_year_of_birth = 2001
 #   - current_year
 #       -set this equal to 2020
+current_year = 2022
 
 
 
